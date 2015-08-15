5334da8db8e7716d7af3bb0da405db044735aedd
Alex Howington <alex.howington@gmail.com>
2015-08-14 22:12:38 -0500

    Use Puma via Procfile

--- Gemfile
+++ Gemfile
@@ -46,3 +46,5 @@ end
 
 
 gem 'rails_12factor', group: :production
+gem 'puma'
+gem 'rack-timeout'
---
+++ config/initializers/timeout.rb
@@ -0,0 +1,3 @@
+Rack::Timeout.timeout = 20  # seconds
+
+Rack::Timeout.unregister_state_change_observer(:logger) if Rails.env.development?
